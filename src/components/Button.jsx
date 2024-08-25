b9a587dbc7ca23a0d1cdfa4b7f09d045870e4d52
Nəzər <n1e2z3e4r5@gmail.com>
2024-08-25 20:38:11 +0400

    refactor(button classname):simplify className

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,36 @@
 function Button({ name, onClick, value, color }) {
-    let classnames =
-        ' flex h-full items-center justify-center rounded-3xl text-2xl transition-all  ';
+    
+    let baseClassnames =
+        'flex h-full items-center justify-center rounded-3xl text-2xl transition-all ' +
+        'sm:hover:ring sm:hover:ring-offset-1 sm:active:ring-offset-0 ';
 
     switch (color) {
         case 'blue':
-            classnames +=
-                ' bg-blue-600 text-white dark:text-white sm:hover:bg-blue-700 sm:hover:ring sm:hover:ring-blue-700 sm:hover:ring-offset-1 sm:hover:ring-offset-blue-700  sm:active:ring-offset-0  ';
+            baseClassnames +=
+                'bg-blue-600 text-white dark:text-white ' +
+                'sm:hover:bg-blue-700 sm:hover:ring-blue-700 sm:hover:ring-offset-blue-700 ';
             break;
 
         case 'gray':
-            classnames +=
-                '   text-black bg-[#d2d3da] sm:hover:bg-[#b4b6bded]  sm:hover:bg-[#b3b4bb] sm:hover:ring sm:hover:ring-[#b3b4bb] sm:hover:ring-offset-[#b3b4bb] dark:bg-[#4f505f] dark:hover:bg-[#606375]  dark:text-white  sm:hover:ring sm:dark:hover:ring-[#606375]  sm:hover:ring-offset-1  sm:dark:hover:ring-offset-[#606375] ] sm:active:ring-offset-0   ';
+            baseClassnames +=
+                'bg-[#d2d3da] text-black ' +
+                'dark:bg-[#4f505f] dark:text-white dark:hover:bg-[#606375] ' + //dark mode
+                'sm:hover:bg-[#b3b4bb] sm:hover:bg-[#b4b6bded] ' + //ligth mode
+                'sm:hover:ring sm:hover:ring-[#b3b4bb] sm:hover:ring-offset-[#b3b4bb] ' + //ligth mode
+                'sm:dark:hover:ring-[#606375] sm:dark:hover:ring-offset-[#606375] ';  //dark mode
             break;
 
         default:
-            classnames +=
-                ' bg-white  text-black bg-white sm:hover:bg-stone-50  sm:hover:ring-stone-50 sm:hover:ring-offset-stone-50    dark:bg-[#2e2f38] sm:dark:hover:bg-[#434554] dark:text-white sm:hover:ring sm:dark:hover:ring-[#434554]  sm:hover:ring-offset-1  sm:dark:hover:ring-offset-[#434554]  sm:active:ring-offset-0  ';
+            baseClassnames +=
+                'bg-white text-black ' +
+                'dark:bg-[#2e2f38] dark:text-white ' +//dark mode 
+                'sm:hover:bg-stone-50 sm:hover:ring-stone-50 sm:hover:ring-offset-stone-50 ' + //light mode
+                'sm:dark:hover:bg-[#434554] sm:dark:hover:ring-[#434554] sm:dark:hover:ring-offset-[#434554] '; //dark mode
             break;
     }
 
     return (
-        <button name={name} onClick={onClick} className={classnames}>
+        <button name={name} onClick={onClick} className={baseClassnames}>
             {value}
         </button>
     );
